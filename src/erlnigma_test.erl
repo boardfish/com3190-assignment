97887f3d1dbbaa6014481959b40114e79d5d6405
Simon Fish <si@mon.fish>
2020-05-14 14:08:13 +0100

    Stub necessary functions

--- src/erlnigma_test.erl
+++ src/erlnigma_test.erl
@@ -1,5 +1,5 @@
 -module(erlnigma_test).
--import(erlnigma, [start/0, ping/2, pong/0]).
+-import(erlnigma, []).
 
 -include_lib("eunit/include/eunit.hrl").
 
@@ -9,8 +9,12 @@ expect(Val) ->
       Other -> {error, Other}
     end.
 
-ping1_test() ->
-    PID = spawn(erlnigma, ping, [1, self()]),
-    ok = expect({ping, PID}),
-    PID ! pong,
-    ok = expect(finished).
+% ping1_test() ->
+%     PID = spawn(erlnigma, ping, [1, self()]),
+%     ok = expect({ping, PID}),
+%     PID ! pong,
+%     ok = expect(finished).
+
+% Tests required:
+% - Some tests to check that Reflector returns the result of frefl(x)
+% - Calling Keyboard(x) with a linked Plugboard sends x to the plugboard and 
